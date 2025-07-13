37fdbda5666a2354953d2df9e4c6c17fac2d7a66
Kartik Dua <kartik.dua@icloud.com>
2025-07-14 03:15:54 +0530

    sort

--- Array/StockBuySell.java
+++ Array/StockBuySell.java
@@ -14,7 +14,13 @@ public class StockBuySell {
         return maxProfit;
     }
     public static void main(String args[]){
-        int price[]={7,1,5,3,6,4};
+        int price[]={7,1,5,3,6,4}; 
+        // Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5. Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+
+        // int price[]={7,6,4,3,1}; 
+        //In this case, no transactions are done and the max profit = 0
+        
         System.out.println(stocks(price));
     }
 }
