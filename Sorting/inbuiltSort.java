77210b680e4281547b44785d122ffc75507e14e7
Kartik Dua <kartik.dua@icloud.com>
2025-07-22 01:35:15 +0530

    inbuilt sort fix

--- Sorting/inbuiltSort.java
+++ Sorting/inbuiltSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class inbuiltSort {
-     public static void printorder(int arr[]){
+    public static void printorder(int arr[]){
         for(int i=0; i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
