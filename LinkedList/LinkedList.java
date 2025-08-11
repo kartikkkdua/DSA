b37b1c3d4b4d65b046ba7ac9db359c96f41dd0ec
Kartik Dua <kartik.dua@icloud.com>
2025-08-12 01:14:45 +0530

    linked list update

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -79,6 +79,120 @@ public class LinkedList {
         newNode.next=temp.next;
         temp.next=newNode;
     }
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if ( size ==1){
+            int val =head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        int val=head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
+    public int removeLast(){
+        if(size==0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if( size ==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        //prev node would be size-2 yes
+        Node prev=head;
+        for(int i=0; i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data; //tail.data
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
+    public int iterativeSearch(int key){
+        Node temp=head;
+        int i =0;
+
+        while(temp != null){
+            if(temp.data==key){ //key found
+                return i;
+            }
+            temp=temp.next;
+            i++;
+        }
+
+        //if key is not found
+        return -1;
+
+    }
+    public int helper(Node head, int key){
+        if(head==null){
+            return -1;
+        }
+        if(head.data==key){
+            return 0;
+        }
+        int idx=helper(head.next,key);
+        if(idx==-1){
+            return -1;
+        }
+        return idx+1;
+    }
+    public int recursiveSearch(int key){
+       return helper(head,key);
+    }
+    public void reverse(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+
+        while(curr != null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev; 
+    }
+    public void deleteNthFromEnd(int n){
+        //calculate size of ll
+        int size=0;
+        Node temp=head;
+        while(temp != null){
+            temp=temp.next;
+            size++;
+        }
+        if(n==size){
+            head=head.next; // remove if head is the one
+            return;
+        }
+        int i=1;
+        int iToFind=size-n;
+        Node prev=head;
+        while(i<iToFind){
+            prev=prev.next;
+            i++;
+        }
+        prev.next=prev.next.next;
+        return;
+    }
+    public Node findMid(Node head){
+        Node slow=head;
+        Node fast=head;
+
+        while( fast != null && fast.next != null){
+            slow=slow.next; //1
+            fast=fast.next.next; //2
+        }
+        return slow; //slow is my mid node here
+    }
+    
     public static void main(String args[]) {
         LinkedList ll = new LinkedList();
         // ll.head = new Node(1);
@@ -89,14 +203,35 @@ public class LinkedList {
         ll.print();
         ll.addFirst(1);
         ll.print();
-        ll.addLast(3);
-        ll.print();
         ll.addLast(4);
         ll.print();
-
-        ll.add(2,9);
+        ll.addLast(5);
         ll.print();
 
-        System.out.println(ll.size);
+        ll.add(2,3);
+        ll.print();
+
+        // System.out.println(ll.size);
+        // ll.removeFirst();
+        // ll.print();
+        // System.out.println(ll.size);
+        
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println(ll.size);
+        
+
+        // System.out.println(ll.iterativeSearch(3));
+        // System.out.println(ll.iterativeSearch(1));
+        // System.out.println(ll.recursiveSearch(3));
+        // System.out.println(ll.recursiveSearch(1));
+
+        // ll.print();
+        // ll.reverse();
+        // ll.print();
+
+        ll.print();
+        ll.deleteNthFromEnd(3);
+        ll.print();
     }
 }
