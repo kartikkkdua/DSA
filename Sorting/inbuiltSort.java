1b61d609da01bb2a73da9301ea7465afc2e26b7d
Kartik Dua <kartik.dua@icloud.com>
2025-08-18 01:17:28 +0530

    fixes

--- Sorting/inbuiltSort.java
+++ Sorting/inbuiltSort.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class inbuiltSort {
-    public static void printorder(int arr[]){
-        for(int i=0; i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+    public static void printOrder(Integer arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+        System.out.print(arr[i] + " ");
     }
+    System.out.println();
+}
     public static void main(String args[]){
         // int arr[]={5,4,1,3,2};
         //sorting in ascending order
@@ -18,8 +18,6 @@ public class inbuiltSort {
         // Arrays.sort(arr,0,2);
         Integer arr[]={5,4,1,3,2};
         Arrays.sort(arr,Collections.reverseOrder());
-        printorder(arr);
-
-
+        printOrder(arr);
     } 
 }
