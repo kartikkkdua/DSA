e4c8333d9532a8b512195406a7b2d75383b8027b
Kartik Dua <kartik.dua@icloud.com>
2025-08-25 14:15:10 +0530

    binary trees added

---
+++ Binary Trees/Part-1/heightTree.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class heightTree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+
+        return Math.max(lh, rh)+1;
+    }
+    public static int count(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lcount=count(root.left);
+        int rcount=count(root.right);
+
+        return lcount+rcount+1;
+    }
+    public static int sum(Node root){
+        if(root==null){
+          return 0;
+        }
+        int ls=sum(root.left);
+        int rs=sum(root.right);
+
+        return ls+rs+root.data;
+
+    }
+    public static void main(String args[]){
+        /*
+                1
+               /  \
+              2    3
+             / \   / \
+            4  5  6  7
+        */
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right= new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+        
+
+        System.out.println("Height of tree : "+height(root));
+        System.out.println("No of nodes count : "+count(root));
+         System.out.println("Sum of Tree : "+sum(root));
+    }
+}
